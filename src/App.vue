83ea2e78684e094f7dbd66a630450d4343b2a20f
mirosha <mpohuy3@gmail.com>
2025-02-25 13:21:08 +0200

    Fix stuff -.-

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,16 @@
 <script setup>
-import { computed, ref } from 'vue';
-let id = 1;
-let items = ref([
-    {id: id++, name: 'Milk', isDone: true },
-    {id: id++, name: 'Bread', isDone: false },
-    {id: id++, name: 'Vodka', isDone: true },
-    {id: id++, name: 'Beer', isDone: false },
-    {id: id++, name: 'Chips', isDone: false },
-]);
-let newItem = ref('');
+import { ref } from 'vue';
+import RouterTabs from './components/RouterTabs.vue';
 
-function add(){
-    if(newItem.value.trim() !== ''){
-        items.value.push({ id: id++, name: newItem.value, isDone: false });
-    }
-    newItem.value = '';
-}
-
-let doneItems = computed(() => items.value.filter(item => item.isDone));
-let toDoItems = computed(() => items.value.filter(item => !item.isDone));
 
 </script>
 <template>
-    <div class="container mt-3">
-        <div class="content">
-
-            <div class="field has-addons">
-                <div class="control is-expanded">
-                    <input class="input" type="text" v-model="newItem" @keypress.enter="add">
-                </div>
-                <div class="control">
-                    <button class="button is-primary" @click="add">Add</button>
-                </div>
-            </div>
-            
-            <h1>All Items</h1>
-            <ul>
-                <li v-for="item in items" :key="item.id">
-                    {{ item.name }}
-                    <input type="checkbox" v-model="item.isDone">
-                </li>
-            </ul>
-
-            <h1>Done Items</h1>
-            <ul>
-                <li v-for="item in doneItems" :key="item.id">
-                    {{ item.name }}
-                    <input type="checkbox" v-model="item.isDone">
-                </li>
-            </ul>
-
-            <h1>ToDo Items</h1>
-            <ul>
-                <li v-for="item in toDoItems" :key="item.id">
-                    {{ item.name }}
-                    <input type="checkbox" v-model="item.isDone">
-                </li>
-            </ul>
-
-        </div>
+    <RouterTabs></RouterTabs>
+    <div class="container">
+        <section class="section">
+            <Suspense>
+                <RouterView></RouterView>
+            </Suspense>
+        </section>
     </div>
 </template>
-<style></style>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
 import './style.scss';
 
 import { createApp } from 'vue';
+import { createWebHashHistory, createWebHistory, createMemoryHistory, createRouter } from 'vue-router';
 
 import App from './App.vue';
 
+import ToDo from './pages/ToDo.vue';
+import Modals from './pages/Modals.vue';
+import ChuckNorris from './pages/ChuckNorris.vue';
+import RickAndMorty from './pages/RickAndMorty.vue';
+
+const routes = [
+  { path: '/', component: ToDo, name: 'ToDo' },
+  { path: '/modals', component: Modals, name: 'Modals' },
+  { path: '/chuck', component: ChuckNorris, name: 'Chuck Norris' },
+  { path: '/rickandmorty', component: RickAndMorty, name: 'Rick And Morty' },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+
 const app = createApp(App);
+app.use(router);
 app.mount('#app');
