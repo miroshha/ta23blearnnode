d6473c0815b42dcdf326f37559734c278eeae2bb
mirosha <mpohuy3@gmail.com>
2025-03-11 14:06:07 +0200

    init commit

--- src/pages/RickAndMorty.vue
+++ src/pages/RickAndMorty.vue
@@ -8,49 +8,72 @@ import PagedPagination from '../components/PagedPagination.vue';
 const characters = ref([]);
 const pagination = ref({});
 let currentPage = ref(1)
+let searchValue = ref('');
+let searchTimeout = null;
+
 await page(currentPage.value)
 
 
-async function getCharacters(url) {
-    let response = await axios.get(url);
-    characters.value.push(...response.data.results);
+async function getCharacters() {
+    let response = await axios.get(`https://rickandmortyapi.com/api/character`, {
+        params: {
+            page: currentPage.value,
+            name: searchValue.value
+        }
+    });
+    characters.value = response.data.results;
     pagination.value = response.data.info;
 }
 
 async function next() {
     currentPage.value++
-    await getCharacters(pagination.value.next);
+    await getCharacters();
 }
 
 async function prev() {
     currentPage.value--
-    await getCharacters(pagination.value.prev);
+    await getCharacters();
 }
 
 async function page(page) {
     currentPage.value = page
-    getCharacters(`https://rickandmortyapi.com/api/character?page=${page}`)
+    getCharacters()
 }
 
-onMounted(() => {
-    document.addEventListener('scroll', () => {
-        if (window.scrollY + window.innerHeight > document.body.clientHeight-300) {
-            if (pagination.value.next) {
-                next()
-            }
-        }
-        // console.log(document.body.clientHeight, window.scrollY + window.innerHeight)
-    })
-})
+async function search() {
+    clearTimeout(searchTimeout)
+    searchTimeout = setTimeout(async () => {
+        await getCharacters()
+    }, 250)
+}
+
+// onMounted(() => {
+//     document.addEventListener('scroll', () => {
+//         if (window.scrollY + window.innerHeight > document.body.clientHeight - 300) {
+//             if (pagination.value.next) {
+//                 next()
+//             }
+//         }
+//         // console.log(document.body.clientHeight, window.scrollY + window.innerHeight)
+//     })
+// })
 
 </script>
 <template>
-    <PagedPagination :pagination="pagination" :current="currentPage" @next="next" @prev="prev" @page="page">
-    </PagedPagination>
+    <div class="field has-addons">
+        <div class="control is-expanded">
+            <input class="input" type="text" placeholder="Search.." @input="search" v-model="searchValue">
+        </div>
+        <div class="control">
+            <button class="button is-info" @click="search">
+                Search
+            </button>
+        </div>
+    </div>
+    <PagedPagination :current="currentPage" :pagination="pagination" @next="next" @prev="prev" @page="page"></PagedPagination>
     <div class="columns is-multiline">
-        <div v-for="character in characters" class="column is-one-quarter">
+        <div class="column is-one-quarter" v-for="character in characters">
             <CharacterCard :character="character"></CharacterCard>
         </div>
     </div>
-
 </template>
